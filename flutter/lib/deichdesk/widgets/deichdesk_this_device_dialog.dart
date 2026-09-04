c7ef1d6f46767d24449835ca9ca1d5afbb354fe7
Greg Deichler <gregdeichler@gmail.com>
2026-09-03 23:18:19 -0400

    Fix This Device binding import

--- flutter/lib/deichdesk/widgets/deichdesk_this_device_dialog.dart
+++ flutter/lib/deichdesk/widgets/deichdesk_this_device_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hbb/common.dart';
-import 'package:flutter_hbb/consts.dart';
+import 'package:flutter_hbb/models/platform_model.dart';
 import 'package:flutter_hbb/models/server_model.dart';
 import 'package:provider/provider.dart';
 
